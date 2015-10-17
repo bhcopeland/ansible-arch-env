0589e360df4c7a10530f247ab9d1f02c6231bdcf
Ben Copeland <ben.copeland@linaro.org>
2015-10-17 12:27:13 +0100

    - Added ansible vault for secerts and included vars file. - Fixed user password not being set with password_hash('sha512') - Added fancontrol, cataylst and intel driver installation - Improved file names and used of var_prompts

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 # ansible-arch-env
 
-To run the playbook: "./ansible_local_setup.sh -i ,"
+To run the playbook: "./ansible_local_setup.sh -i ,". The comma is there by design ;-).
 
 Passwords are asked on prompt. If passwords are skipped the step will be skipped.
 
 System wide vars set in group_vars/all
 
-OwnCloud keeps all my config in sync across mulitple computers.
+OwnCloud keeps all my config in sync across multiple computers.
+
+# Vault
+
+ansible-vault create secrets.yml
+
+secrets.yml:
+ - user_password: hiddenpassword
+ - owncloud_password: hiddenpassword
--- ansible_local_setup.sh
+++ ansible_local_setup.sh
@@ -1,32 +1,42 @@
-#!/usr/bin/ansible-playbook
+#!/usr/bin/ansible-playbook --ask-vault-pass
 ---
 # Install and setup local env
 
-- name: Install and setup local env
-  gather_facts: false
-  hosts: localhost
-  vars_prompt:
-    user_password: "Set your choose a user password?"
-    owncloud_password: "Set your owncloud client passwd?"
 
-  tasks:
-    # - name: configure pacman/packages
-    #   include: configure-pacman.yml
+-  hosts: localhost
+   gather_facts: false
+   vars_files:
+     - secrets.yml
+   vars_prompt:
+     configure_catalyst: "Configure Catalyst?"
+     configure_laptop: "Configure Intel?"
+     setup_packages: "Setup pacman/yaourt?"
 
-    - name: configure yaort
-      include: configure-yaourt.yml
+   tasks:
+      - name: configure pacman/packages
+        include: configure_pacman.yml
+        when: setup_packages.0 is defined
 
-    - name: enable services
-      include: enable-services.yml
+      - name: configure yaort
+        include: configure_yaourt.yml
+        when: setup_packages.0 is defined
 
-    - name: username setup
-      include: configure-user.yml
+      - name: configure bootup services and files
+        include: configure_services.yml
 
-    - name: sudo setup
-      include: configure-sudo.yml
+      - name: username setup
+        include: configure_user.yml
 
-    - name: setup owncloud
-      include: configure-owncloud.yml
+      - name: sudo setup
+        include: configure_sudo.yml
 
-    # - name: setup symlinks
-    #   include: configure-symlinks.yml
+      - name: configure owncloud client
+        include: configure_owncloud.yml
+
+      - name: configure catalyst (desktop)
+        include: configure_catalyst.yml
+        when: configure_catalyst.0 is defined
+
+      - name: configure intel (laptop)
+        include: configure_intel.yml
+        when: configure_intel.0 is defined
