28ed0940c1e377004b7d63050ad962e139414eed
Ben Copeland <ben.copeland@linaro.org>
2017-10-03 18:19:42 +0100

    AUR: Fix missing AUR packages and add a -Qs test check pacman: fix missing packages ansible: fix var_prompt usage make nfs mounts switchable remove infinality fonts config

--- ansible_local_setup.sh
+++ ansible_local_setup.sh
@@ -9,24 +9,30 @@
    vars_files:
      - secrets.yml
    vars_prompt:
-     configure_xf86-video-amdgpu: "Configure xf86-video-amdgpu?"
-     configure_intel: "Configure xf86-video-intel?"
-     setup_yaourt: "Setup aur packages?"
+     - name: "configure_video_amdgpu"
+       prompt: "Configure xf86-video-amdgpu?"
+     - name: "configure_intel"
+       prompt: "Configure xf86-video-intel?"
+     - name: "setup_aur"
+       prompt: "Setup aur packages?"
+     - name: "setup_pacman"
+       prompt: "Setup pacman packages?"
 
    tasks:
       - name: configure video_drivers
         include: configure_video_drivers.yml
-        when: (configure_intel.0 is defined) or (configure_xf86-video-amdgpu is defined)
+        when: (configure_intel.0 is defined) or (configure_video_amdgpu.0 is defined)
 
       - name: user/sudo and folder creation
         include: configure_user.yml
 
       - name: configure pacman/packages
         include: configure_pacman.yml
+        when: setup_pacman.0 is defined
 
       - name: configure aur
         include: configure_aur.yml
-        when: setup_yaourt.0 is defined
+        when: setup_aur.0 is defined
 
       - name: configure systemd services and etc files
         include: configure_services.yml
